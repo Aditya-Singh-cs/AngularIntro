f9eae785f743f87bb848e652525b691ac971e9ed
Aditya-Singh-cs <aditya76596@gmail.com>
2025-03-24 11:04:48 +0530

    Open Website by clicking on logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  // Optional if you want styles
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hello from BridgeLabz';
-  logoUrl = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';
+  logoUrl: string = 'https://www.bridgelabz.com/assets/images/BridgeLabz%20New%20Logo.svg';
+
+  openBridgeLabz() {
+    window.open('https://www.bridgelabz.com', '_blank');
+  }
 }
